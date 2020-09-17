8f24f8a9c18765732dc431ad21e9149e13edb170
Alexander Appel <alexandera2@gso.schule.koeln>
2020-09-17 12:43:58 +0200

    init

---
+++ Test/Meth.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Test
+{
+    internal static class Meth
+    {
+        public static double? Median(IEnumerable<int> input)
+        {
+            if (!CanHandle(input))
+            {
+                return null;
+            }
+            var sorted = SortCollection(input);
+
+            return null;
+        }
+
+        public static double? Mittelwert(IEnumerable<int> input)
+        {
+            if (!CanHandle(input))
+            {
+                return null;
+            }
+
+            double sum = 0;
+            foreach(var entry in input)
+            {
+                sum += entry;
+            }
+
+            return sum / input.Count();
+        }
+
+        public static double? Modus(IEnumerable<int> input)
+        {
+            if (!CanHandle(input))
+            {
+                return null;
+            }
+            return null;
+        }
+
+        private static IEnumerable<int> SortCollection(IEnumerable<int> input)
+        {
+            return input.OrderBy(x => x);
+        }
+
+        private static bool CanHandle(IEnumerable<int> input)
+        {
+            return input != null ? input.Any() : false;
+        }
+    }
+}
---
+++ Test/MethTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Test
+{
+    public class MethTest
+    {
+        private readonly List<int> _unsortedOdd = new List<int> { 2, 1, 3 };
+        private readonly List<int> _unsortedEven = new List<int> { 2,1, 3, 4 };
+        
+        private readonly List<int> _nullList = null;
+        private readonly List<int> _emptyList = new List<int>();
+
+        private readonly List<int> _sortedOdd = new List<int> { 1, 2, 3 };
+        private readonly List<int> _sortedEven = new List<int> { 1, 2, 3 };
+
+        [Fact]
+        public void Mittelwert_UnsortedOddList_ShouldBeTwo()
+        {
+            //Act
+            var res = Meth.Mittelwert(_unsortedOdd);
+            
+            //Assert
+            Assert.Equal(2, res);
+        }
+
+        [Fact]
+        public void Mittelwert_UnsortedEvenList_ShouldBeTwoPointFive()
+        {
+            //Act
+            var res = Meth.Mittelwert(_unsortedEven);
+            
+            //Assert
+            Assert.Equal(2.5, res);
+        }
+
+        [Fact]
+        public void Mittelwert_Null_ReturnsNull()
+        {
+            //Act
+            var res = Meth.Mittelwert(_nullList);
+            
+            //Assert
+            Assert.Null(res);
+        }
+
+        [Fact]
+        public void Mittelwert_EmptyList_ReturnsNull()
+        {
+            //Act
+            var res = Meth.Mittelwert(_emptyList);
+            
+            //Assert
+            Assert.Null(res);
+        }
+
+        [Fact]
+        public void Mittelwert_SortedOdd_Two()
+        {
+            //Act
+            var res = Meth.Mittelwert(_sortedOdd);
+
+            //Assert
+            Assert.Equal(2,res);
+        }
+
+        [Fact]
+        public void Mittelwert_SortedEven_TwoPointFive()
+        {
+            //Act
+            var res = Meth.Mittelwert(_sortedEven);
+            
+            //Assert
+            Assert.Equal(2.5, res);
+        }
+    }
+}
